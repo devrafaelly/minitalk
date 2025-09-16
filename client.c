338c97e98f6ed15e20968f5aee3c4f6cf312e3ae
Rafaelly Diniz <rafaellypoliveira@gmail.com>
2025-09-16 19:25:32 -0300

    feat: implement minitalk client with signal transmission

--- client.c
+++ client.c
@@ -15,25 +15,47 @@
 #include <signal.h>
 #include <sys/types.h>
 
+# define BIT_ZERO SIGUSR1
+# define BIT_ONE SIGUSR2
+
+static void	send_signal(char c, int server_pid)
+{
+	int	bit;
+	int	i;
+	i = 0;
+	while (i < 8)
+	{
+		bit = (c >> i) & 1;
+		if (bit == 1)
+			kill(server_pid, BIT_ONE);
+		else
+			kill(server_pid, BIT_ZERO);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
+	char	*message;
 	pid_t	server_pid;
 
 	if (argc != 3)
-		kill ; // seria isso? verificar
-	// tranformar PID em numero
+	{
+		ft_printf("Informar PID do server e mensagem\n");
+		return (1);
+	}
 	server_pid = (pid_t)ft_atoi(argv[1]);
-	// verificar se PID é valido
 	if (server_pid <= 0)
 	{
 		ft_printf("Erro: PID inválido\n");
 		return (1);
 	}
-	// transforma caracteres em bits
-	argv[2];
-	// envia para server
-	while ()
-	{
-		kill(pid_t pid, int sig);
+	message = argv[2];
+	while (*message)
+	`{
+		send_signal(*message, server_pid);
+		message++;
 	}
+	send_signal(*message, server_pid);
+	return (0);
 }
